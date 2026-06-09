537213d065026085b8d62d6ceb209d0756703c38
PatriciaSolano <solanopatricia772@gmail.com>
2026-06-08 20:25:23 -0500

    Elimina emojis del script

--- scripts/procesamiento.py
+++ scripts/procesamiento.py
@@ -1,18 +1,18 @@
 import pandas as pd
 
-print("🔵 INICIO DEL PROCESO")
+print("INICIO DEL PROCESO")
 
 df = pd.read_csv('data/dataset.csv')
 
-print("\n📌 Dataset original:")
+print("\nDataset original:")
 print(df)
 
 df = df.fillna(0)
 
-print("\n🧹 Dataset limpio:")
+print("\nDataset limpio:")
 print(df)
 
 df.to_csv('output/dataset_limpio.csv', index=False)
 
-print("\n✅ Archivo exportado correctamente")
-print("🔵 FIN DEL PROCESO")
+print("\nArchivo exportado correctamente")
+print("FIN DEL PROCESO")
